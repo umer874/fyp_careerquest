1ee6a7e8c164330af4ae4fcc4e4c190324ec28eb
Anas <anashamid992@gmail.com>
2025-06-05 18:15:12 +0500

    Describe what you changed

--- app/_shared/utils/server-side-helper.ts
+++ app/_shared/utils/server-side-helper.ts
@@ -150,7 +150,7 @@ async function GetTokensFromCookies() {
 
 export {
   redirectBasedOnRole,
-  //apiCallWithToken,
+  apiCallWithToken,
   refreshServerToken,
   GetCookieUser,
   GetTokensFromCookies,
