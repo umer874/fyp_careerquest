30df20e64c2a14cb146ecbd39d3b4c89589eb176
Anas <anashamid992@gmail.com>
2025-06-14 17:56:13 +0500

    Fix TypeScript linting and Sass warnings

--- app/(pages)/general/insights/page.tsx
+++
@@ -1,9 +0,0 @@
-import JobsWrapper from "components/job/wrapper";
-import { GetJobsServerCall } from "services/job";
-
-async function Jobs() {
-  const response = await GetJobsServerCall();
-  return <JobsWrapper response={response} />;
-}
-
-export default Jobs;
