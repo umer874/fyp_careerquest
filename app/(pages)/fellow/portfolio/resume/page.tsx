51ab7f1ac516b0928d60b82e620a50d109bab7a3
Anas <anashamid992@gmail.com>
2025-06-28 00:11:17 +0500

    Initial commit

--- app/(pages)/fellow/portfolio/resume/page.tsx
+++
@@ -1,9 +0,0 @@
-import UploadResumePage from "components/fellow/portfolio/resume";
-import React from "react";
-import { GetCookieUser } from "utils/server-side-helper";
-const Resume =  () => {
-  //const user =  GetCookieUser();
-  // return <UploadResumePage />;
-};
-
-export default Resume;
